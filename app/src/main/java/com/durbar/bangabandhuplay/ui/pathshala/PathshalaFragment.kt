600c0ec65e839a7e2245de20f4c7b92a9e492956
NurHossain9 <nur.hossain@durbar.live>
2024-01-24 09:59:34 +0600

    add PathshalaFragment & TunesFragment

---
+++ app/src/main/java/com/durbar/bangabandhuplay/ui/pathshala/PathshalaFragment.kt
@@ -0,0 +1,21 @@
+package com.durbar.bangabandhuplay.ui.pathshala
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.durbar.bangabandhuplay.R
+
+
+class PathshalaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pathshala, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/durbar/bangabandhuplay/ui/tunes/TunesFragment.kt
@@ -0,0 +1,21 @@
+package com.durbar.bangabandhuplay.ui.tunes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.durbar.bangabandhuplay.R
+
+
+class TunesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tunes, container, false)
+    }
+
+}
